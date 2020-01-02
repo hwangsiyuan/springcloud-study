bf81c556075781dc9f76be773e9f528df208d505
hwangsy <siyuan.huang@fenbeitong.com>
2020-01-02 16:48:21 +0800

    eureka

--- eureka-server/src/main/java/com/hussein/App.java
+++ eureka-server/src/main/java/com/hussein/App.java
@@ -2,21 +2,14 @@ package com.hussein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class App {
-
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- simple-provider-user/src/main/java/com/hussein/App.java
+++ simple-provider-user/src/main/java/com/hussein/App.java
@@ -2,8 +2,10 @@ package com.hussein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class App {
 
     public static void main(String[] args) {
