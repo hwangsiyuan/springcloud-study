4e869a3bc1921eabfaab60ff11a4e2df00525644
hwangsy <siyuan.huang@fenbeitong.com>
2020-01-03 15:59:06 +0800

    meta

--- simple-consumer-user/src/main/java/com/hussein/controller/UserController.java
+++ simple-consumer-user/src/main/java/com/hussein/controller/UserController.java
@@ -2,10 +2,14 @@ package com.hussein.controller;
 
 import com.hussein.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * <p>Title: UserController</p>
@@ -21,8 +25,16 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("findUserById/{id}")
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/findUserById/{id}")
     public User findById(@PathVariable("id") long id) {
         return restTemplate.getForObject("http://127.0.0.1:8000/findUserById/" + id, User.class);
+    }
+
+    @GetMapping("/showInfo")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("simple-provider-user");
     }
 }
