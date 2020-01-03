d4e996ea760d364984b7e0158b88e210096c8044
hwangsy <siyuan.huang@fenbeitong.com>
2020-01-03 20:19:32 +0800

    ribbon without eureka

---
+++ ribbon-consumer-user-without-eureka/src/main/java/com/hussein/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.hussein.controller;
+
+import com.hussein.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * <p>Title: UserController</p>
+ * <p>Description: </p>
+ * <p>Company: www.hussein.com</p>
+ *
+ * @author hwangsy
+ * @date 2020/1/2 10:20 AM
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/findUserById/{id}")
+    public User findById(@PathVariable("id") long id) {
+        return restTemplate.getForObject("http://simple-provider-user/findUserById/" + id, User.class);
+    }
+
+    @GetMapping("/chooseService")
+    public ServiceInstance chooseService() {
+        return loadBalancerClient.choose("simple-provider-user");
+    }
+}
