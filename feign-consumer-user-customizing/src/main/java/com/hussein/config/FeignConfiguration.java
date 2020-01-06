0af2c72a28464772d600b6a5869138b50e031a5d
hwangsy <siyuan.huang@fenbeitong.com>
2020-01-06 11:05:59 +0800

    feign customizing

--- feign-consumer-user-customizing/src/main/java/com/hussein/config/FeignConfiguration.java
+++ feign-consumer-user-customizing/src/main/java/com/hussein/config/FeignConfiguration.java
@@ -1,6 +1,8 @@
 package com.hussein.config;
 
 import feign.Contract;
+import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +20,16 @@ public class FeignConfiguration {
     @Bean
     public Contract feignContract() {
         return new Contract.Default();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        //这里记录所有，根据实际情况选择合适的日志level
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
+        return new BasicAuthRequestInterceptor("admin","123456");
     }
 }
