28fd3c7629f60780d3b4623f4a6038bb6da6008f
hwangsy <siyuan.huang@fenbeitong.com>
2020-01-07 19:12:34 +0800

    turbine stream

--- hystrix-turbine-mq/src/main/java/com/hussein/App.java
+++ hystrix-turbine-mq/src/main/java/com/hussein/App.java
@@ -1,9 +1,12 @@
 package com.hussein;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -15,5 +18,16 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @SuppressWarnings("all")
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
